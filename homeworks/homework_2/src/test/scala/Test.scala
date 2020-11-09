1e19645b799554dc913ae549f940bbbe601dbfd8
changingnicknamebrb <56918117+changingnicknamebrb@users.noreply.github.com>
2020-11-09 23:16:08 +0500

    Update Test.scala

--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -8,5 +8,42 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(1, 10) == 33)
+            assert(Exercises.sumOfDivBy3Or5(3, 6) == 14)
+            assert(Exercises.sumOfDivBy3Or5(1, 4) == 3)
+            assert(Exercises.sumOfDivBy3Or5(1, 15) == 60)
+        }
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(10) == Seq(2, 5))
+            assert(Exercises.primeFactor(6) == Seq(2, 3))
+            assert(Exercises.primeFactor(20) == Seq(2, 5))
+            assert(Exercises.primeFactor(50) == Seq(2, 5))
+            assert(Exercises.primeFactor(14) == Seq(2, 7))
+            assert(Exercises.primeFactor(44) == Seq(2, 11))
+        }
+
+        'test_sumScalars - {
+            assert(Exercises.sumScalars(Exercises.Vector2D(0, 0),
+                Exercises.Vector2D(0, 1),
+                Exercises.Vector2D(1, 0),
+                Exercises.Vector2D(1, 1)) == 1.0)
+        }
+
+        'test_sumCosines - {
+            assert(math.abs(Exercises.sumCosines(Exercises.Vector2D(1, 0),
+                Exercises.Vector2D(0, 1),
+                Exercises.Vector2D(1, 0),
+                Exercises.Vector2D(1, 1)) - 0.7071) < 5e-4)
+        }
+
+        val balls: Map[String, (Int, Double)] =
+            Map(
+                "Aluminum" -> (3,   2.6889), "Tungsten" ->  (2,   19.35), "Graphite" ->  (12,  2.1),   "Iron" ->      (3,   7.874)
+            )
+
+        'test_weight - {
+            assert(Exercises.sortByHeavyweight(balls) == Seq("Aluminum", "Tungsten", "Iron", "Graphite"))
+        }
     }
 }
