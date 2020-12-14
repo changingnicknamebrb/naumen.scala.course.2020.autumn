d4c6dec59f80cdcfdf04b5f07d4fe86d8f58760c
changingnicknamebrb <56918117+changingnicknamebrb@users.noreply.github.com>
2020-12-15 00:44:04 +0500

    Update Test.scala

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,34 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
+        'test_bool_format - {
             val trueStr = "правда"
+
             assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter2(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter3(true) == trueStr)
+            assert(Exercises.prettyBooleanFormatter1(5) == 5.toString)
+            assert(Exercises.prettyBooleanFormatter2(5) == 5.toString)
+            assert(Exercises.prettyBooleanFormatter3(5) == 5.toString)
+        }
+        'test_max - {
+            val seq = Seq(1, 3, 4, 2, 5, 6, 9)
+            val seqempty = Seq()
+            assert(Exercises.max1(seq) == 9)
+            assert(Exercises.max2(seq) == Seq(9))
+            assert(Exercises.max3(seq) == Some(9))
+            assert(Exercises.max1(seqempty) == Int.MinValue)
+            assert(Exercises.max2(seqempty) == Seq())
+            assert(Exercises.max3(seqempty) == None)
+        }
+
+
+        'test_sum - {
+            val x = 4
+            val y = 5
+            assert(Exercises.sum1(x, y) == x + y)
+            assert(Exercises.sum2(x, y) == x + y)
+            assert(Exercises.sum3(x, y) == x + y)
         }
     }
 }
